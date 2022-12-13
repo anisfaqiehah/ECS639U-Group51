3da545abd1f19cbac4b9ab680a08bfb5c2b9f38e
esthersnchz <73084063+esthersnchz@users.noreply.github.com>
2022-12-13 18:32:02 +0000

    Add files via upload

---
+++ frontend2/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.scss'
+import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+createApp(App).mount('#app')
