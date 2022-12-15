75a9325c8bddcfc85fb4c4c7fc84112e3eba465d
anisfaqiehah <78006058+anisfaqiehah@users.noreply.github.com>
2022-12-15 13:29:18 +0000

    Update App.vue

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,17 +1,64 @@
 
 <template>
-  <ul>
-    <li><router-link :to="{name:'Home'}">Home</router-link></li>
-    <li><router-link :to="{name:'About'}">About</router-link></li>
-    <li><router-link :to="{name:'Login'}">Login/Register</router-link></li>
-    <li><router-link :to="{name:'Profile'}">Profile</router-link></li>
-    <li><router-link :to="{name:'Bids'}">Bids</router-link></li>
+  <div id="wrapper">
+    <nav class="navbar fixed-top navbar-dark bg-dark">
+  <div class="container-fluid">
+    <h3>Group 51</h3>
+      <ul class="nav">
+    <li class="nav-item">
+      <a class="nav-link active"><router-link :to="{name:'Home'}">Home</router-link></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" ><router-link :to="{name:'Login'}"> Login/Register </router-link></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link"><router-link :to="{name:'Profile'}"> Profile </router-link></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link"><router-link :to="{name:'Bids'}"> Bids </router-link></a>
+    </li>
   </ul>
-  <router-view />
+  </div>
+</nav>
+
+  <section class="section">
+    <router-view/>
+  </section>
+
+  <Footer/>
+  
+  </div>
+
+
+<!-- <div>
+  <ul>
+      <router-link :to="{name:'Home'}">Home </router-link>
+      <router-link :to="{name:'Login'}"> Login/Register </router-link>
+      <router-link :to="{name:'Profile'}"> Profile </router-link>
+      <router-link :to="{name:'Bids'}"> Bids </router-link>
+      <router-link :to="{name:'Add'}"> Add </router-link>
+  <router-view/>
+</ul>
+</div> -->
 </template>
 
-<script setup>
+
+<script>
 import { RouterView, RouterLink } from 'vue-router'
+import Footer from '@/components/footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Footer
+  }
+}
 </script>
+
+<style scoped>
+h3{
+  color: aliceblue;
+}
+</style>
 
 
