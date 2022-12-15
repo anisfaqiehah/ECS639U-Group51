40ef1f6004c90fd9c30c20af30600e7661c6f40a
anisfaqiehah <78006058+anisfaqiehah@users.noreply.github.com>
2022-12-15 16:40:50 +0000

    Update router.js

--- Frontend/src/plugins/router.js
+++ Frontend/src/plugins/router.js
@@ -5,7 +5,7 @@ const routes = [
         {path: '/login', name: 'Login', component: () => import('@/views/Login.vue')},
         {path: '/register', name: 'Register', component: () => import('@/views/Register.vue')},
         {path: '/profile', name: 'Profile', component: () => import('@/views/Profile.vue')},
-        {path: '/bids', name: 'Bids', component: () => import('@/views/Bids.vue')},
+        {path: '/bids', name: 'Bids', component: () => import('@/views/items.vue')},
         {path: '/add', name: 'Add', component: () => import('@/views/AddItem.vue')},
     ]
 
