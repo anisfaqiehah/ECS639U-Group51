287fef64687cc2a4cd8344d85ae4904328ff988e
anisfaqiehah <78006058+anisfaqiehah@users.noreply.github.com>
2022-12-15 16:03:06 +0000

    Update router.js

--- Frontend/src/plugins/router.js
+++ Frontend/src/plugins/router.js
@@ -2,11 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
         {path: '', name: 'Home', component: () => import('@/views/Home.vue')},
-        {path: '/about', name: 'About', component: () => import('@/views/About.vue')},
         {path: '/login', name: 'Login', component: () => import('@/views/Login.vue')},
         {path: '/register', name: 'Register', component: () => import('@/views/Register.vue')},
         {path: '/profile', name: 'Profile', component: () => import('@/views/Profile.vue')},
         {path: '/bids', name: 'Bids', component: () => import('@/views/Bids.vue')},
+        {path: '/add', name: 'Add', component: () => import('@/views/AddItem.vue')},
     ]
 
 const router = createRouter({
