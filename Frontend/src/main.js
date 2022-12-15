d362987ba9d93c465a51bc0132320015bc6b174f
anisfaqiehah <78006058+anisfaqiehah@users.noreply.github.com>
2022-12-15 13:28:59 +0000

    Update main.js

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
-import '@/style.css'
-import App from '@/App.vue'
-import router from '@/plugins/router'
+import './style.css'
+import App from './App.vue'
+import router from './plugins/router'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 createApp(App).use(router).mount('#app')
