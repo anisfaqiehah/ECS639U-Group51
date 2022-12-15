3ee9c9c99bb594c102434075c162baa6b4fa32e3
anisfaqiehah <78006058+anisfaqiehah@users.noreply.github.com>
2022-12-15 13:34:00 +0000

    Update Bids.vue

--- Frontend/src/views/Bids.vue
+++ Frontend/src/views/Bids.vue
@@ -1,26 +1,35 @@
 <template>
     <h1>Bids</h1>
-    <table>
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
+    <table id="firstTable">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Phone</th>
+      <th>Profession</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="row in rows">
+      <td>{{row.id}}</td>
+      <td>{{row.name}}</td>
+      <td>{{row.phone}}</td>
+      <td>{{row.profession}}</td>
+    </tr>
+  </tbody>
 </table>
 </template>
 
-<style>
-table, th, td {
-  border:1px solid white;
+<script >
+export default{
+  data(){
+    return{
+      id:1,
+      name: 'Anis',
+      phone:0,
+      profession: 'hello',
+      row: [],
+    }
+  }
 }
-</style>
+</script>
