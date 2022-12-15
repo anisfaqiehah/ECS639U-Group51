02af01a76260ca12b44453e1be52def6578e637c
anisfaqiehah <78006058+anisfaqiehah@users.noreply.github.com>
2022-12-15 13:33:46 +0000

    Delete About.vue

--- Frontend/src/views/About.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <h1>About</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et aliquam urna, vel fermentum justo. Curabitur ac semper tellus. Aenean non eros faucibus, pretium massa vel, ultrices dui. Mauris sed mi porttitor, placerat lorem quis, rhoncus dolor. Maecenas consectetur, risus in malesuada dictum, urna urna luctus nisl, sodales pulvinar orci felis at magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed in odio lectus. Vivamus auctor eros non sapien cursus gravida. Aenean finibus lorem at nisi pretium euismod.</p>
-</template>
