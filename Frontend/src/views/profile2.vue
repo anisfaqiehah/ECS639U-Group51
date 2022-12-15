8a9fe75b3539fcdfa2fc2eca19c1dab1521c5446
esthersnchz <ec20808@qmul.ac.uk>
2022-12-15 16:33:24 +0000

    profile pages now rendering correctly from the database

--- Frontend/src/views/profile2.vue
+++ Frontend/src/views/profile2.vue
@@ -1,129 +1,130 @@
 <template>
 
-  <div >
-    <div v-for="profile in profiles" :key="profile.id" class="editable-field">
-      <template v-if="editedFieldId === profile.id">
-        <div class="card text-center border-success mb-4 " id="profile-card">
-             
-        <form>
-          <h2 class="text-success m-3 ">Edit profile</h2>
-          
-          <div class="form-group m-3">
-            <input type="text" v-model="profile.username" :ref="`field${profile.id}`" class="form-control"/>
-          </div>
-          <div class="form-group m-3">
-            <input type="text" v-model="profile.email" :ref="`field${profile.id}`" class="form-control"/>
-          </div>
-          <div class="form-group  m-3">
-             <input type="text" v-model="profile.city" :ref="`field${profile.id}`" class="form-control" placeholder="City"/>
-          </div>
-          <div class="form-group m-3">
-             <input type="date" v-model="profile.dob" :ref="`field${profile.id}`" class="form-control" placeholder="Date of birth: YYYY/MM/DD" />
-          </div>
-          <div class="form-group m-3">
-             <input type="text" v-model="profile.bio" :ref="`field${profile.id}`" class="form-control"  placeholder="User bio"/>
-          </div>
-          <div class="form-group m-3">
-             <label for="img">Select image:</label>
-            <input type="file" id="img" name="img" accept="image/*">  
-          </div>
-           <button type="button" class="btn btn-success" @click.prevent="toggleEdit" @click="updateProfile(profile)" id="song-btn">Save
-        </button>
-        </form>
-         
-        </div>
-       
-      </template>
-      <template v-else>
-        <div class="card text-center border-success mb-4" id="profile-card">
-                <div class="row g-0">
-               <h1>Profile information</h1>
-
-        </div>
-              <div class="row g-0">
-              <div class="col-md-4">
-      <img src="https://www.shareicon.net/data/2016/09/15/829453_user_512x512.png" class="img-fluid rounded-start" style="width:80%">
-       <h3 class="editable-text text-success ">
-          {{ profile.username }}
-        </h3>
-    </div>
-    <div class="col-md-8">
-        <div class="card-body">
-
-         <ul class="list-group list-group-flush">
-       <li class="list-group-item">{{ profile.email }}</li>
-        <li class="list-group-item"> {{ profile.city }}</li>
-        <li class="list-group-item"> {{ profile.dob }}</li>
-        <li class="list-group-item"> {{ profile.bio }}</li>
-        </ul>
-      </div>
-      </div>
-      </div>
-      <div class="row g-0">
-               <button type="button" class="btn btn-outline-success" @click.prevent="toggleEdit(profile.id)" id="song-btn">Edit</button>
-
-        </div>
-      </div>
-      </template>
-    </div>
-    
-     
-  </div>
-</template>
-
-<script>
-export default {
-   data(){
-         return{
-                editedFieldId: null,  
-               profiles:[],
+    <div >
+      <div v-for="profile in profiles" :key="profile.id" class="editable-field">
+        <template v-if="editedFieldId === profile.id">
+          <div class="card text-center border-success mb-4 " id="profile-card">
                
-         }
-   },
-   methods:{  
-    async fetchProfile(){
-      //Perform ajax request to fetch the list of items
-      let response= await fetch("http://localhost:8000/profile/")
-      let data= await response.json()
-      this.profiles=data.profiles
-      console.log(this.profiles)
-    },
-   
-    toggleEdit(id) {
-      if (id) {
-        this.editedFieldId = id;
-        this.$nextTick(() => {
-          if (this.$refs["field" + id]) {
-            this.$refs["field" + id][0].focus();
-          }
-        });
-      } else {
-        this.editedFieldId = null;
-      }
-    }, 
-async updateProfile(profile) {
-      const putData = {
-        username:profile.username,
-        email:profile.email,
-        city: profile.city,
-        dob: profile.dob,
-        bio:profile.bio,
-      };
-            const response = await fetch("http://localhost:8000/profile/"+profile.id, {method:"PUT",
-            headers: {
-              "Content-Type": "application/json",
-            },
+          <form v-on:submit.prevent="submitForm">
+            <h2 class="text-success m-3 ">Edit profile</h2>
+            
+            <div class="form-group m-3">
+              <input type="text" v-model="profile.username" :ref="`field${profile.id}`" class="form-control"/>
+            </div>
+            <div class="form-group m-3">
+              <input type="text" v-model="profile.email" :ref="`field${profile.id}`" class="form-control"/>
+            </div>
+            <div class="form-group  m-3">
+               <input type="text" v-model="profile.city" :ref="`field${profile.id}`" class="form-control"/>
+            </div>
+            <div class="form-group m-3">
+               <input type="date" v-model="profile.dob" :ref="`field${profile.id}`" class="form-control" />
+            </div>
+            <div class="form-group m-3">
+               <input type="text" v-model="profile.bio" :ref="`field${profile.id}`" class="form-control" />
+            </div>
+            <div class="form-group m-3">
+               <label for="img">Select image:</label>
+              <input type="file" id="img" name="img" accept="image/*">  
+            </div>
+             <button type="button" class="btn btn-success" @click.prevent="toggleEdit" @click="updateSong(song)" id="song-btn">Save
+          </button>
+          </form>
            
-            body:JSON.stringify(putData),
-            });
-            const data = await response.json();
-            },
-   },
-   beforeMount(){
-            this.fetchProfile()
-              },
-}
-</script>
-
-<style >
-</style>
+          </div>
+         
+        </template>
+        <template v-else>
+          <div class="card text-center border-success mb-4" id="profile-card">
+                  <div class="row g-0">
+                 <h1>Profile information</h1>
+  
+          </div>
+                <div class="row g-0">
+                <div class="col-md-4">
+        <img src="https://www.shareicon.net/data/2016/09/15/829453_user_512x512.png" class="img-fluid rounded-start" style="width:80%">
+         <h3 class="editable-text text-success ">
+            {{ profile.username }}
+          </h3>
+      </div>
+      <div class="col-md-8">
+          <div class="card-body">
+  
+           <ul class="list-group list-group-flush">
+         <li class="list-group-item">{{ profile.email }}</li>
+          <li class="list-group-item"> {{ profile.city }}</li>
+          <li class="list-group-item"> {{ profile.dob }}</li>
+          <li class="list-group-item"> {{ profile.bio }}</li>
+          </ul>
+        </div>
+        </div>
+        </div>
+        <div class="row g-0">
+                 <button type="button" class="btn btn-outline-success" @click.prevent="toggleEdit(profile.id)" id="song-btn">Edit</button>
+  
+          </div>
+        </div>
+        </template>
+      </div>
+      
+       
+    </div>
+  </template>
+  
+  <script>
+  
+  
+  export default {
+     data(){
+           return{
+                 editedFieldId: null,
+                 infos: [],
+                 profiles:[{id:1, 
+                  username:"esther001",
+                  email:'example@email.com', 
+                  city:"London",
+                  dob:"06/12/2000",
+                  bio:"HI my name is x and I am from London, I study at QMUL and my hobbies are..."},
+                 
+                 ]
+           }
+     },
+     methods:{  
+      async fetchInfo() {
+        // Perform an Ajax request to fetch the info of a user
+        let response = await fetch("http://localhost:8000/profile/")
+        let data = await response.json()
+        this.info = data.profiles
+        },
+      
+        async toggleEdit(info){
+          try{
+          // Send a request to API to update the info
+          const response = await this.$http.put(`http://localhost:8000/profile/${info.id}/`, {
+              email: info.title,
+              dob: info.dob,
+              pic: info.pic
+          });
+          // Get the index of the info being updated
+          let infoIndex = this.infos.findIndex(t => t.id === info.id);
+          // Reset the info array with the new data of the updated info
+          this.infos = this.infos.map((info) => {
+              if(this.infos.findIndex(t => t.id === info.id) === infoIndex){
+                  return response.data;
+              }
+              return info;
+          });
+          }catch(error){
+          // Log any error
+          console.log(error);
+          }
+          },
+      
+     }
+  }
+  </script>
+  
+  <style >
+  
+  
+  </style>
--- auctions/settings.py
+++ auctions/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"] 
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173","http://localhost:8080","http://localhost:5174"] 
 
 ROOT_URLCONF = 'auctions.urls'
 
