195f0d3f0a1263aa41ec8a95a52ee8ce12e5e724
anisfaqiehah <78006058+anisfaqiehah@users.noreply.github.com>
2022-12-16 14:35:31 +0000

    Update Register.vue

--- Frontend/src/views/Register.vue
+++ Frontend/src/views/Register.vue
@@ -3,13 +3,13 @@
     <h1>Register</h1>
     <form v-on:submit.prevent="submitForm">
   <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <label for="user">Username</label>
+    <input type="text" class="form-control" id="user" aria-describedby="emailHelp" placeholder="Username">
+    <small id="userHelp" class="form-text text-muted">Make your username unique.</small>
   </div>
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <label for="pass">Password</label>
+    <input type="password" class="form-control" id="pass" placeholder="Password">
   </div><br>
   <button type="p-2 submit" class="btn btn-primary">Submit</button>
 </form>
