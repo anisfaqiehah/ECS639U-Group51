4a64eb54d5d424a484582c75f1311407f2c0460d
anisfaqiehah <78006058+anisfaqiehah@users.noreply.github.com>
2022-12-16 13:12:12 +0000

    Update Home.vue

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
-<div class="jumbotron">
+<div id="master">
+ <div class="p-5 m-5 jumbotron bg-secondary bg-opacity-10 rounded-pill" >
   <h1 class="display-4">Welcome to our auction site!</h1>
   <p class="lead">This is a simple web app created by Group 51. Feel free to roam around through the navbar on top.</p>
-  <hr class="my-4">
-  <p>Special thanks to : Esther, Agnes and Anis for making this happen. Have a great holiday!</p>
+  <hr class="my-3">
+  <div class="btn bg-warning">Special thanks to</div>
+  <p><mark class="fw-bold">Agnes, Esther and Anis</mark>for making this happen. Have a great holiday!</p>
 
     <div class="container">
     <img src="@/assets/agnes.png" alt="agnes" class="rounded">
     <img src="@/assets/esther.png" alt="esther" class="rounded">
     <img src="@/assets/anis.png" alt="anis" class="rounded">
     </div>
+</div>
 </div>
 </template>
 
@@ -18,3 +21,8 @@ export default {
   }
 </script>
 
+<style>
+mark{
+  background: none;
+}
+</style>
